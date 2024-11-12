32df484e4188fa9062a126951adf0d2f127946ea
serge1997 <serge.gogolo12@gmail.com>
2024-11-11 23:12:56 -0300

    list side dish by menu item

--- app/Main/MenuitemSideDish/Action/MenuitemSideDishList.php
+++ app/Main/MenuitemSideDish/Action/MenuitemSideDishList.php
@@ -1,12 +1,24 @@
 <?php
 namespace App\Main\MenuitemSideDish\Actions;
 
+use App\Http\Resources\MenuitemSideDishResource;
+use App\Main\Menuitem\Repository\MenuitemRepository;
+use App\Main\Menuitem\Repository\MenuitemRepositoryInterface;
 use App\Main\MenuitemSideDish\Repository\MenuitemSideDishRepositoryInterface;
 
 class MenuitemSideDishList
 {
     public function __construct(
-        private MenuitemSideDishRepositoryInterface $menuitemSideDishRepository
+        private MenuitemSideDishRepositoryInterface $menuitemSideDishRepository,
+        private MenuitemRepositoryInterface $menuitemRepository
     )
     {}
+
+    public function listAllByMenuitem(int $menuitem_id)
+    {
+        $menuitem = $this->menuitemRepository->find($menuitem_id);
+        return MenuitemSideDishResource::collection(
+            $this->menuitemSideDishRepository->findAllByMenuitem($menuitem)
+        );
+    }
 }
